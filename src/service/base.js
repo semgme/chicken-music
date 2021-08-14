41ba2cc9d6e458c31d8b4f71e242294db843eebd
peter <2593910610@qq.com>
2021-08-14 17:29:09 +0800

    up

--- src/service/base.js
+++ src/service/base.js
@@ -4,7 +4,7 @@ import axiosRetry from 'axios-retry'
 axiosRetry(axios, { retries: 5 })
 
 const ERR_OK = 0
-const baseURL = process.env.NODE_ENV === 'production' ? '120.24.74.136' : '/'
+const baseURL = process.env.NODE_ENV === 'production' ? '120.24.74.136:9000' : '/'
 
 axios.defaults.baseURL = baseURL
 
